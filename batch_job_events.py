c305a65696087822154e8f29a650dcbc00330cba
Lanqing Yuan <yuanlq@uchicago.edu>
2023-10-23 15:36:44 -0500

    update

--- batch_job_events.py
+++ batch_job_events.py
@@ -47,17 +47,43 @@ class Submit(object):
 
         # Modify here for the log name
         utilix.batchq.submit_job(
-            jobstring, log='/home/yuanlq/.tmp_job_submission/gain_crisis/process_events%s.log'%(run_id), partition='xenon1t', qos='xenon1t',
+            jobstring, log='/home/yuanlq/.tmp_job_submission/manual_process/sr0_bkg_unfinished/process_events%s.log'%(run_id), partition='lgrandi', qos='lgrandi',
             account='pi-lgrandi', jobname=jobname,
-            delete_file=True, dry_run=False, mem_per_cpu=40000,
-            container='xenonnt-development.simg',
+            delete_file=True, dry_run=False, mem_per_cpu=75000,
+            container='xenonnt-2023.10.1.simg',
             cpus_per_task=1)
 
 p = Submit()
 
 # Modify here for the runs to process
-loop_over = np.array(['051538', '051537', '051536', '051535', '051534', '051880', '051879', '051878', '051877', '051866'])
+#"""SR0 BKG unfinshed
+loop_over = np.array(['031513', '031512',
+       '031507', '031256', '031243', 
+       '031235', '031224', '031218', '031217', '031215',
+       '031196', '031195', '031185', 
+       '031173', '031161', 
+       '031152', '031146', '031133',
+       '031087', '031076', '031072', '031031', '030175', '029891',
+       '029798', '029743'])
+#"""
+"""SR0 Rn220 unfinshed batch 0
+loop_over = np.array(['024075', '024072', '024068', '024065', '024062'])
+"""
+"""SR0 Rn220 unfinshed batch 1
+loop_over = np.array(['024031', '024028', '024023', '024018', '024015', '024012',
+       '023991', '023988', '023985', '023982', '023936', '023927',
+       '023925', '023922', '023919', '023892', '023889', '023881',
+       '023831', '023828'])
+"""
+"""What's that bkg?
+loop_over=np.array(['031513', '031512', '031507', '031256', '031243', '031235',
+       '031224', '031218', '031217', '031215', '031196', '031195',
+       '031185', '031173', '031161', '031152', '031146', '031133',
+       '031087', '031076', '031072', '031031', '030175', '029891',
+       '029798', '029743'])
+"""
+#loop_over=np.array(['029743','029798','029891','030175','031031','031072','031076', '031087', '031133','031152','031185','031217','031507'])
 
 print('Runs to process: ', len(loop_over))
 
-p.execute(loop_over=loop_over, max_num_submit=21, nmax=10000)
+p.execute(loop_over=loop_over, max_num_submit=100, nmax=10000)
--- process_events.py
+++ process_events.py
@@ -2,24 +2,38 @@ import strax
 import straxen
 import numpy as np
 import sys
+import gc
 import cutax
 
 print("Finished importing, now start to load context.")
 # Modify below for the strax.storage path
-st = cutax.xenonnt_offline(output_folder='/project2/lgrandi/xenonnt/processed')
-PATH_TO_REPO = "/project2/lgrandi/cimental/corrections/" 
-st.set_config(
-{"gain_model": f"list-to-array://xedocs://pmt_area_to_pes?as_list=True&detector=tpc&run_id=plugin.run_id&version=v10_test&attr=value&db=local_folder&db__path={PATH_TO_REPO}"}
-)
-st.storage.append(strax.DataDirectory('/scratch/midway2/yuanlq/downloads/gain_crisis', readonly=True))
+st = cutax.xenonnt_offline(output_folder='/scratch/midway3/yuanlq/download/sr0_bkg_unfinished')
+#PATH_TO_REPO = "/project2/lgrandi/cimental/corrections/" 
+#st.set_config(
+#{"gain_model": f"list-to-array://xedocs://pmt_area_to_pes?as_list=True&detector=tpc&run_id=plugin.run_id&version=v10_test&attr=value&db=local_folder&db__path={PATH_TO_REPO}"}
+#)
+#st.storage = [strax.DataDirectory('/scratch/midway3/yuanlq/download/sr0_bkg_unfinished')]
 _, runid = sys.argv
 print("Loaded the context successfully, and the run id to process:", runid)
 
 print("Start to make data")
 
-st.make(runid,"event_info_double")
+st.make(runid,"peaklets")
+gc.collect()
+st.make(runid,"event_basics")
+gc.collect()
+st.make(runid,"event_info")
+gc.collect()
+st.make(runid,"event_pattern_fit")
+gc.collect()
+st.make(runid,"event_area_per_channel", save=("event_area_per_channel"))
+gc.collect()
+st.make(runid,"event_ms_naive")
+gc.collect()
+st.make(runid,"cuts_basic")
+gc.collect()
 
-if st.is_stored(runid,"event_info"):
+if st.is_stored(runid,"cuts_basic"):
     print('Finished all.')
 else:
     print('Failed processing...')
