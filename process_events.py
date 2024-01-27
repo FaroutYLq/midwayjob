7f6ece2456d1a5141293955fbd9e9e0fab842c87
Lanqing Yuan <yuanlq@uchicago.edu>
2024-01-26 21:24:19 -0600

    cuts_basic

--- process_events.py
+++ process_events.py
@@ -10,7 +10,7 @@ print("Finished importing, now start to load context.")
 st = cutax.xenonnt_online(output_folder='/project/lgrandi/xenonnt/processed')
 _, runid = sys.argv
 
-to_process = ['event_positions_nv','peaklet_classification', 'merged_s2s', 'peak_basics', 'peak_positions_gcn', 'peak_positions_mlp', 'peak_positions_cnn', 'peak_proximity', 'energy_estimates', 'corrected_areas', 'event_positions', 'event_basics', 'event_info', 'distinct_channels', 'event_top_bottom_params', 'event_shadow', 'event_pattern_fit', 'event_ms_naive', 'event_ambience', 'event_area_per_channel']
+to_process = ['cuts_basic']
 
 for dt in to_process:
     print('Making %s'%(dt))
